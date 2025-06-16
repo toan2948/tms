a6cc2877ffd64b36eaa16531fab7034536236b65
toan0176 <xuantoan2948@gmail.com>
2025-06-16 19:31:19 +0200

    change keys

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -6,8 +6,10 @@ export async function createClient() {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    // process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
