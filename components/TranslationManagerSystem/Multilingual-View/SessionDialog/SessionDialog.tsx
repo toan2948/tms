2fd89ed929e0999f1164896b15088b0520504c06
toan0176 <xuantoan2948@gmail.com>
2025-07-06 23:25:09 +0200

    change dialog ui

--- components/TranslationManagerSystem/Multilingual-View/SessionDialog/SessionDialog.tsx
+++ components/TranslationManagerSystem/Multilingual-View/SessionDialog/SessionDialog.tsx
@@ -55,50 +55,59 @@ export function SessionDialog({ open, onClose }: SessionDialogProps) {
     <Dialog onClose={handleClose} open={open}>
       <DialogTitle textAlign={"center"}>Save Session?</DialogTitle>
 
-      <Box
-        sx={{
-          // margin: "10px",
-          padding: "10px",
-        }}
-      >
-        <Typography>Save these changes:</Typography>
-
+      {sessionData && sessionData.length > 0 ? (
         <Box
           sx={{
-            border: "1px solid black",
-
             padding: "10px",
-            marginBottom: "10px",
           }}
         >
-          <List sx={{ pt: 0 }}>
-            {sessionData.map((item, index) => (
-              <ListItem
-                key={index}
-                sx={{
-                  borderRadius: "5px",
-                }}
-              >
-                <Typography color={item.color}> {item.label}</Typography>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-        <Stack direction={"row"} justifyContent={"flex-end"}>
-          <Button
-            variant='outlined'
+          <Typography>Save these changes:</Typography>
+
+          <Box
             sx={{
-              marginRight: "10px",
+              border: "1px solid black",
+
+              padding: "10px",
+              marginBottom: "10px",
             }}
-            onClick={handleClose}
           >
-            Cancel
-          </Button>
-          <Button variant='contained' onClick={updateDB}>
-            Save
-          </Button>
-        </Stack>
-      </Box>
+            <List sx={{ pt: 0 }}>
+              {sessionData.map((item, index) => (
+                <ListItem
+                  key={index}
+                  sx={{
+                    borderRadius: "5px",
+                  }}
+                >
+                  <Typography color={item.color}> {item.label}</Typography>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+          <Stack direction={"row"} justifyContent={"flex-end"}>
+            <Button
+              variant='outlined'
+              sx={{
+                marginRight: "10px",
+              }}
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
+            <Button variant='contained' onClick={updateDB}>
+              Save
+            </Button>
+          </Stack>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            padding: "10px",
+          }}
+        >
+          No changes
+        </Box>
+      )}
     </Dialog>
   );
 }
